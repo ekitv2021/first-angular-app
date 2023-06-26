a45805f6a7848227873e44bf7e4f283f50594dd0
ekwinder <ekwinder@itvedant.com>
2023-06-26 20:48:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core"
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({    
+    selector:'app',
+    templateUrl: "app.component.html",
+    styleUrls: ["app.component.css"]
 })
-export class AppComponent {
-  title = 'first-app';
+
+export class AppComponent{
+    //to define the variable, no need to use var, let or const
+    //here datatype is assigned implicitly
+    title = "this is my first app"; 
+    num = 89 
+    
+    //here datatype is assigned explicitly
+    // title:any = "string";
+
+    //[] are used to define array,
+    //collection of multiple referred using same variable name
+    colors = ["red","green","blue","yellow","purple","pink","orange"];
+
+    marks: number[] = [56,34,65,46,5,23,90,46];
 }
